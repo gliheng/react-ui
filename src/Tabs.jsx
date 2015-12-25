995807162bf21e9e141ad7d77d97513abc7963f7
Amadeus <gliheng@gmail.com>
2015-12-25 11:27:11 +0800

    create Tab

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -15,12 +15,20 @@ let Tabs = React.createClass({
 
     renderTabbar: function (items) {
         return items.map((tab, i)=> {
-            var isActive = tab.id == this.state.curTab;
-            var className = 'Tabs-Bar-Item';
-            if (isActive) {
-                className += ' active';
-            }
-            var id = this._items[i].id;
+            return this.renderTabbarItem(tab, i);
+        });
+    },
+
+    renderTabbarItem(tab, i) {
+        var isActive = tab.id == this.state.curTab;
+        var className = 'Tabs-Bar-Item';
+        if (isActive) {
+            className += ' active';
+        }
+
+        var itemConfig = this._items[i];
+        if (itemConfig) {
+            var {id} = itemConfig;
             if (id) {
                 className += ` ${id}-Tab`;
             }
@@ -33,12 +41,13 @@ let Tabs = React.createClass({
                         onClick={this.closeTab.bind(null, i, tab.id, this._items[i])}>&times;</a>
                 );
             }
-            return (
-                <div className={className} data-id={tab.id} data-idx={i} key={i} onClick={this.onTabClick}>
-                    <span>{tab.label}</span>{closeBtn}
-                </div>
-            );
-        });
+        }
+
+        return (
+            <div className={className} data-id={tab.id} data-idx={i} key={i} onClick={this.onTabClick}>
+                <span>{tab.label}</span>{closeBtn}
+            </div>
+        );
     },
 
 	closeTab(idx, id, tab, evt) {
@@ -70,12 +79,16 @@ let Tabs = React.createClass({
     onTabClick(evt) {
         var idx = parseInt(evt.currentTarget.dataset.idx),
             id = evt.currentTarget.dataset.id,
-            {tabClicked} = this.props;
-        if (typeof tabClicked == 'function') {
-            id = tabClicked(id, this._items[idx]) || id;
-        }
-        if (typeof id == 'string') {
-            this.setTab(id);
+            {createTab, tabClicked} = this.props;
+        if (id == 'add') {
+            createTab();
+        } else {
+            if (typeof tabClicked == 'function') {
+                id = tabClicked(id, this._items[idx]) || id;
+            }
+            if (typeof id == 'string') {
+                this.setTab(id);
+            }
         }
     },
 
@@ -91,19 +104,9 @@ let Tabs = React.createClass({
         });
     },
 
-    render() {
-        var className = 'Tabs',
-            {props, state} = this;
-
-        if (props.className) {
-            className += ' ' + props.className;
-        }
-
-        var items = [],
-            curTab = state.curTab,
-            element,
-            firstElement;
-        React.Children.forEach(props.children, (c, i)=> {
+    extract(curTab) {
+        var element, firstElement, items = [];
+        React.Children.forEach(this.props.children, (c, i)=> {
             // key always exist, id my not
             let id = c.props.id || (''+i);
             if (id == curTab) {
@@ -128,15 +131,36 @@ let Tabs = React.createClass({
             element = firstElement;
         }
 
+        return [element, items];
+    },
+
+    render() {
+        var className = 'Tabs',
+            {props, state} = this;
+
+        if (props.className) {
+            className += ' ' + props.className;
+        }
+
+        // extract item from tabs
+        var curTab = state.curTab;
+        var [contentElement, items] = this.extract(curTab);
+
         // cloneWithProps does not transfer key or ref to the cloned element.
         var barItems = this.renderTabbar(items),
             content;
-        if (element) {
-            content = React.addons.cloneWithProps(element, {
+        if (contentElement) {
+            content = React.addons.cloneWithProps(contentElement, {
                 ref: 'activeContent',
                 key: curTab,
                 parent: this
             });
+        }
+
+        // optional add button to create more tabs
+        var addTab;
+        if (this.props.createTab) {
+            addTab = this.renderTabbarItem({label: '╋', id: 'add'});
         }
 
         var toolBtns = <div className="Tabs-Bar-ToolBtns">{this.props.toolBtns}</div>;
@@ -144,6 +168,7 @@ let Tabs = React.createClass({
             <div id={this.props.id} className={className}>
                 <div className="Tabs-Bar">
                     {barItems}
+                    {addTab}
                     {toolBtns}
                 </div>
                 <div className="Tabs-Content">{content}</div>
